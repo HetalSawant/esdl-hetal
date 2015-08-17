835b962266987b78f226d83009559ed4af258c7a
HetalSawant <hetalsawant71@gmail.com>
2015-08-17 11:20:58 +0530

    Update calc.cpp
    updated in branch

--- calc.cpp
+++ calc.cpp
@@ -1,9 +1,11 @@
 // This file is to create a simple code for calculator.
 
 
-//This is the new version of file.
+//This is the new version of file
+// Editing in branch sawant.
 #include<iostream.h>
 int main()
 {
   cout<<"This is calculator application.";
+  cout<<"Updated in branch: sawant.";
 }
